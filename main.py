7dcc938d0756286033f9cb48912ed3727df5e79b
Nosha08 <noah.rozin@outlook.com>
2024-11-20 20:38:29 -0500

    Your commit message

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from transformers import pipeline
+import scipy
+import torch
+
+app = Flask(__name__)
+
+if torch.cuda.is_available():
+    device = 0
+else:
+    device = -1
+
+synthesiser = pipeline("text-to-audio", "facebook/musicgen-small", device=device)
+
+music = synthesiser("80s rock music", forward_params={"do_sample": True})
+
+scipy.io.wavfile.write("musicgen_out1.wav", rate=music["sampling_rate"], data=music["audio"])
+
+@app.route("/")
+def hello_world():
+    return "<p>Sigma Music Inc.</p>"
+
+if __name__ == "__main__": 
+    app.run(debug=True)
