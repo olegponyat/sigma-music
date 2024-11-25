eecdb80bcf3fa60fcf319e0d2f06cd4d98ff3771
olegponyat <113192651+olegponyat@users.noreply.github.com>
2024-11-25 15:36:04 -0500

    yes jit

---
+++ sigma_music/src/components/MainPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="centered-container">
+    <div ><em class="quote">"The future of music is right here"</em></div>
+    <div class="create-chat">
+      <router-link to="/create-chat">click here</router-link>
+      <span> to create your first chat</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.centered-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+  position: relative;
+}
+
+.quote {
+  font-size: 5rem;
+  font-weight: bolder;
+  margin-bottom: 1rem;
+  position: relative;
+  top: -10%;
+}
+
+.create-chat {
+  font-size: 1rem;
+}
+
+.create-chat a {
+  color: #007bff;
+  text-decoration: none;
+}
+
+.create-chat a:hover {
+  text-decoration: underline;
+}
+</style>
