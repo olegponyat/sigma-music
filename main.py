7fea7166afd03a2f7af88635563712b12554b379
Nosha08 <noah.rozin@outlook.com>
2024-11-28 19:20:38 -0500

    a

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template, jsonify
 from transformers import pipeline
 from scipy.io import wavfile
 import scipy
@@ -20,38 +20,29 @@ else:
 
 description = "country music"
 plot_filename = "spectrogram.png"
+audio_file = "musicgen_out.wav"
 
-def generate(description, plot_filename):
+def generate(description, plot_filename, audio_file):
     synthesiser = pipeline("text-to-audio", "facebook/musicgen-small", device=device)
 
     music = synthesiser(description, forward_params={"do_sample": True})
     print(music["audio"])
     print(music["sampling_rate"])
 
-    audio_file = "musicgen_out4.wav"
     scipy.io.wavfile.write(audio_file, rate=music["sampling_rate"], data=music["audio"])
 
     samplerate, data = wavfile.read('./musicgen_out.wav')
     print(f'Samplerate: {samplerate}')
-    print(f'Data: {data, data.shape}')
+    print(f'Data: {data, type(data)}')
     display(audio_file, plot_filename)
 
-def display(audio_file, plot_filename):
-    """ length = 29
-    time = np.linspace(0., length, data.shape[0])
-    plt.plot(time, data, label="Amplitude")
-    plt.legend()
-    plt.xlabel("Time [s]")
-    plt.ylabel("Amplitude")
-    plt.show()
- """
-    
+def display(audio_file, plot_filename):  
     y, sr = librosa.load(audio_file)
     fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 6)) 
     
     D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
     librosa.display.specshow(D, y_axis='linear', x_axis='time', sr=sr, ax=ax[0])
-    
+
     ax[0].set(title='Linear-frequency power spectrogram', ylabel='Frequency [Hz]')
     ax[0].label_outer()
     
@@ -67,14 +58,16 @@ def display(audio_file, plot_filename):
     return plot_filename
     
 
-@app.route("/")
+@app.route("/",  methods=["GET"])
 def home():
-    return "<p>Sigma Music Inc.</p>"
+    return render_template("home.html")
 
 @app.route("/music", methods=["GET"])
 def music():
-    generate(description, plot_filename)
-    return send_file(plot_filename, mimetype='image/png')
+    generate(description, plot_filename, audio_file)
+    data = wavfile.read(audio_file)
+    print(data)
+    return render_template("visual.html", spectrogram=plot_filename, data=data[1].tolist())
 
 if __name__ == "__main__": 
     app.run(debug=True)
